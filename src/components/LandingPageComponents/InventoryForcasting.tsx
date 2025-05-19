4327054ed3873ef248204ef8c159c3d994b3f799
ahad <syedabdulahad@innova360.io>
2025-05-19 19:26:30 +0500

    fixed overflow

--- src/components/LandingPageComponents/InventoryForcasting.tsx
+++ src/components/LandingPageComponents/InventoryForcasting.tsx
@@ -1,7 +1,7 @@
 function InventoryForcasting() {
   return (
     <div className="flex flex-col items-center justify-center mt-20 md:mt-40 lg:mt-40 overflow-x-hidden">
-        <div className="text-4xl md:text-5xl lg:text-5xl font-bold text-[#0F172A] fonts text-center gap-1.5 flex flex-col">
+        <div className="text-4xl px-5 md:text-5xl lg:text-5xl font-bold text-[#0F172A] fonts text-center gap-1.5 flex flex-col">
             <div>Intelligent Inventory <span className="text-[var(--primary-color)]">Forecasting</span></div>
             <img src="/underline.svg" className="md:ml-96 lg:ml:96 m-auto" alt="underline" />
             {/* <img src="/arrow2.svg" alt="arrow" className="md:hidden lg:hidden absolute z-10 right-0 size-1/12"/> */}
